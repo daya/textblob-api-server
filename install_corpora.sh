be7d3ed7682b3e0bc0dfb4e1607c03975e002812
Daya Sharma <daya.sharma+github@gmail.com>
2017-04-27 18:54:41 -0500

    Upgrading to latest TextBlob

--- install_corpora.sh
+++ install_corpora.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export TEXTBLOB_VERSION=0.7.1
+export TEXTBLOB_VERSION=0.12.0
 cd /tmp && git clone https://github.com/sloria/TextBlob.git && cd /tmp/TextBlob && git checkout $TEXTBLOB_VERSION && python download_corpora.py && rm -rf /tmp/TextBlob && find $HOME/nltk_data -name '*.zip' | xargs rm
